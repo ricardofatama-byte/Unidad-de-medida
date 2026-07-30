da322963ea0d4ff38b3497804af0f6af98410721
Ricardo <ricardojraf1999@gmail.com>
2026-07-30 11:49:08 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,70 @@
+import { netsuiteRequest } from "./oauth.js";
+import data from "./unidades_conversion.json" assert { type: "json" };
+import "dotenv/config";
+
+async function enviarMasivo() {
+    let obj = {};
+
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        if (!obj[element.nameType]) {
+            obj[element.nameType] = {
+                name: element.nameType,
+                isInactive: false,
+                externalId: element.nameType,
+                uom: {
+                    items:[]
+                }
+            };
+            let temp = {
+                "unitName": element.name,
+                "pluralName": element.pluralName,
+                "abbreviation": element.abv,
+                "pluralAbbreviation": element.pluralAbv,
+                "baseUnit": element.unitBase == "T"  ? true : false,
+                "conversionRate": Number(element.tasa),
+                // "externalId": "UM_99999_UN"
+            }
+
+            obj[element.nameType].uom.items.push(temp);
+        }else{
+            let temp = {
+                "unitName": element.name,
+                "pluralName": element.pluralName,
+                "abbreviation": element.abv,
+                "pluralAbbreviation": element.pluralAbv,
+                "baseUnit": element.unitBase == "T"  ? true : false,
+                "conversionRate": Number(element.tasa),
+                // "externalId": "UM_99999_UN"
+            }
+            obj[element.nameType].uom.items.push(temp);
+        }
+    }
+    // console.log("Unidades agrupadas por tipo:", Object.keys(obj).length);
+    // console.log("el primer valor oño", obj["UM_1002580"])
+    // console.log("el primer valor", obj["UM_1002580"].uom)
+    // delete obj["UM_1015423"]
+
+    // console.log("Unidades agrupadas por tipo después de eliminar UM_1015423:", Object.keys(obj));
+    // console.log("esto es lo que se viene ", obj["UM_1015423"])
+    // console.log("OBJ:", obj);
+    let  valores = Object.values(obj);
+
+    // for (let i = 0; i < valores.length; i++) {
+    //     const unidad = valores[i];
+
+    //     console.log("Enviando unidad", i + 1);
+    //     console.log("Enviando unidad", unidad);
+
+    //     try {
+    //         const r = await netsuiteRequest(valores[i]);
+    //         console.log("OK:", r.id);
+    //     } catch (e) {
+    //         console.log("Error en registro", i + 1, e);
+    //     }
+    // }
+
+    console.log("Proceso terminado", valores.length, "unidades enviadas.");
+}
+
+enviarMasivo();
---
+++ oauth.js
@@ -0,0 +1,82 @@
+import crypto from "crypto";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./llaves.env" });
+
+function encode(str) {
+    return encodeURIComponent(str)
+        .replace(/\!/g, "%21")
+        .replace(/\*/g, "%2A")
+        .replace(/\'/g, "%27")
+        .replace(/\(/g, "%28")
+        .replace(/\)/g, "%29");
+}
+
+function generateOAuthSignature(method, url, params, consumerSecret, tokenSecret) {
+    const sortedParams = Object.keys(params)
+        .sort()
+        .map(key => `${encode(key)}=${encode(params[key])}`)
+        .join("&");
+
+    const baseString = [
+        method.toUpperCase(),
+        encode(url),
+        encode(sortedParams),
+    ].join("&");
+
+    const signingKey = `${encode(consumerSecret)}&${encode(tokenSecret)}`;
+
+    return crypto
+        .createHmac("sha256", signingKey)
+        .update(baseString)
+        .digest("base64");
+}
+
+export async function netsuiteRequest(body) {
+    const method = "POST";
+    const url = "https://11541296.suitetalk.api.netsuite.com/services/rest/record/v1/unitsType";
+
+    const oauth_timestamp = Math.floor(Date.now() / 1000).toString();
+    const oauth_nonce = crypto.randomBytes(16).toString("hex");
+
+    const oauthParams = {
+        oauth_consumer_key: process.env.CONSUMER_KEY,
+        oauth_token: process.env.TOKEN_ID,
+        oauth_signature_method: "HMAC-SHA256",
+        oauth_timestamp,
+        oauth_nonce,
+        oauth_version: "1.0",
+    };
+
+    const signature = generateOAuthSignature(
+        method,
+        url,
+        oauthParams,
+        process.env.CONSUMER_SECRET,
+        process.env.TOKEN_SECRET
+    );
+
+    const authHeader = `OAuth realm="${process.env.ACCOUNT_ID}",` +
+        `oauth_consumer_key="${oauthParams.oauth_consumer_key}",` +
+        `oauth_token="${oauthParams.oauth_token}",` +
+        `oauth_signature_method="HMAC-SHA256",` +
+        `oauth_timestamp="${oauth_timestamp}",` +
+        `oauth_nonce="${oauth_nonce}",` +
+        `oauth_version="1.0",` +
+        `oauth_signature="${encode(signature)}"`;
+
+    const headers = {
+        "Authorization": authHeader,
+        "Content-Type": "application/json",
+        "Cookie": "NS_ROUTING_VERSION=LAGGING",
+    };
+
+    try {
+        const res = await axios.post(url, body, { headers });
+        return res.data;
+    } catch (e) {
+        console.error("NetSuite Error:", e.response?.data || e.message);
+        throw e;
+    }
+}
